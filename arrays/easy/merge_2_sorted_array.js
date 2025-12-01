da7fc9d16c776f92cc2f8edef5476fb2788cd600
Suryakanta Swain <suryakantaswain606@gmail.com>
2025-12-01 22:20:35 +0530

    comments in code is quite good.

---
+++ arrays/easy/merge_2_sorted_array.js
@@ -0,0 +1,67 @@
+// Two sorted input arrays
+let list1 = [1, 2, 3, 7];
+let list2 = [2, 5, 6];
+
+// Output array where we will store the merged sorted result
+let merged = [];
+
+// Pointers to track positions in list1 and list2
+let pointer1 = 0; // index for list1
+let pointer2 = 0; // index for list2
+
+// Total number of elements in the merged array
+let totalLength = list1.length + list2.length;
+
+// Loop exactly totalLength times to fill the merged array
+for (let writeIndex = 0; writeIndex < totalLength; writeIndex++) {
+  if (
+    pointer2 === list2.length ||
+    (pointer1 < list1.length && list1[pointer1] <= list2[pointer2])
+  ) {
+    merged[writeIndex] = list1[pointer1++];
+  } else {
+    merged[writeIndex] = list2[pointer2++];
+  }
+}
+
+console.log(merged);
+
+//or
+
+// Two sorted input arrays
+// let list1 = [1, 2, 3, 7];
+// let list2 = [2, 5, 6];
+
+// // Output array where we will store the merged sorted result
+// let merged = [];
+
+// // Pointers to track positions in list1 and list2
+// let pointer1 = 0; // index for list1
+// let pointer2 = 0; // index for list2
+
+// // Total number of elements in the merged array
+// let totalLength = list1.length + list2.length;
+
+// // Loop exactly totalLength times to fill the merged array
+// for (let writeIndex = 0; writeIndex < totalLength; writeIndex++) {
+//   // If list1 is exhausted, take from list2
+//   if (pointer1 === list1.length) {
+//     merged[writeIndex] = list2[pointer2++];
+//     continue;
+//   }
+
+//   // If list2 is exhausted, take from list1
+//   if (pointer2 === list2.length) {
+//     merged[writeIndex] = list1[pointer1++];
+//     continue;
+//   }
+
+//   // Normal case: pick the smaller element between list1 and list2
+//   if (list1[pointer1] <= list2[pointer2]) {
+//     merged[writeIndex] = list1[pointer1++];
+//   } else {
+//     merged[writeIndex] = list2[pointer2++];
+//   }
+// }
+
+// console.log(merged);
